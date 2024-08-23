aea1b103ed02ca211d3a0111249c39c390462056
ChathuraD1392 <chathuradilshan1392@gmail.com>
2024-08-23 11:10:03 +0530

    create book model

--- bookzone-api/server/api/models.py
+++ bookzone-api/server/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title= models.CharField(max_length=25)
+    author= models.CharField(max_length=20)
+    category= models.CharField(max_length=10)
+    description= models.CharField(max_length=100)
+    year= models.IntegerField()
+    cover=models.ImageField(upload_to='photos/', null=True, blank=True)
+
+
+def __str__(self):
+    return self.title
